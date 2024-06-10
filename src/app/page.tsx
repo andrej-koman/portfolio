5ed362d9954030259996aa41596cc11e10850d69
andrej-koman <andrej.koman123@gmail.com>
2024-06-10 14:36:24 +0200

    feat: Added toltips to my links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   IconType,
   SiGithub,
@@ -5,52 +7,79 @@ import {
   SiMaildotru,
 } from '@icons-pack/react-simple-icons'
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from '@/components/ui/tooltip'
+
 export default function Home() {
-  const links: { icon: IconType; href: string }[] = [
-    {
-      icon: SiMaildotru,
-      href: 'mailto:andrejkoman@hotmail.com',
-    },
-    {
-      icon: SiGithub,
-      href: 'https://github.com/andrej-koman',
-    },
-    {
-      icon: SiLinkedin,
-      href: 'https://www.linkedin.com/in/andrej-koman-424883235/',
-    },
-  ]
+  const links: {
+    icon: IconType
+    href: string
+    title: string
+    mail?: string
+  }[] = [
+      {
+        icon: SiMaildotru,
+        href: 'mailto:andrejkoman@hotmail.com',
+        title: 'andrejkoman@hotmail.com',
+      },
+      {
+        icon: SiGithub,
+        href: 'https://github.com/andrej-koman',
+        title: 'andrej-koman',
+      },
+      {
+        icon: SiLinkedin,
+        href: 'https://www.linkedin.com/in/andrej-koman-424883235/',
+        title: 'Andrej Koman',
+      },
+    ]
 
   return (
-    <div className="p-10 text-xl leading-[1.7] w600:p-[30px] w600:text-lg w400:p-5 w400:text-base">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
-        doloremque dolores accusamus rerum hic unde!
-      </p>
+    <TooltipProvider delayDuration={250}>
+      <div className="p-10 text-xl leading-[1.7] w600:p-[30px] w600:text-lg w400:p-5 w400:text-base">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+          doloremque dolores accusamus rerum hic unde!
+        </p>
 
-      <br />
+        <br />
 
-      <p>
-        This is the windowed portfolio neobrutalism template. Check the{' '}
-        <a
-          className="font-bold underline"
-          target="_blank"
-          href="https://github.com/neobrutalism-templates/windowed-portfolio"
-        >
-          github repo
-        </a>{' '}
-        for more info.
-      </p>
+        <p>
+          This is the windowed portfolio neobrutalism template. Check the{' '}
+          <a
+            className="font-bold underline"
+            target="_blank"
+            href="https://github.com/neobrutalism-templates/windowed-portfolio"
+          >
+            github repo
+          </a>{' '}
+          for more info.
+        </p>
 
-      <div className="mr-auto mt-10 flex w-full flex-wrap items-center gap-10">
-        {links.map((link, id) => {
-          return (
-            <a target="_blank" key={id} href={link.href}>
-              <link.icon title="" />
-            </a>
-          )
-        })}
+        <div className="mr-auto mt-10 flex w-full flex-wrap items-center gap-10">
+          {links.map((link, id) => {
+            return (
+              <Tooltip key={id}>
+                <TooltipTrigger asChild>
+                  <a target="_blank" href={link.href}>
+                    <link.icon title="" />
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent sideOffset={5} align="center">
+                  <div className="flex items-center gap-2">
+                    <link.icon title="" />
+                    <span>{link.title}</span>
+                  </div>
+                </TooltipContent>
+              </Tooltip>
+            )
+          })}
+        </div>
       </div>
-    </div>
+    </TooltipProvider>
   )
 }
